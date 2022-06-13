6578b825a8a17395f3302c565b8f8d43d09c5117
Jakub Mitura <jakub.mitura14@gmail.com>
2022-06-13 11:13:44 +0200

    tidied up directories
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

---
+++ preprocessing/manageMetaData.py
@@ -0,0 +1,55 @@
+import transformsForMain
+import pandas as pd
+
+def getMonaiSubjectDataFromDataFrame(row):
+        """
+        given row from data frame prepares Subject object from it
+        """
+        subject= {"adc": str(row['adc'])        
+        , "cor":str(row['cor'])
+        , "hbv":str(row['hbv'])
+        , "sag":str(row['sag'])
+        , "t2w":str(row['t2w'])
+        , "isAnythingInAnnotated":row['isAnythingInAnnotated']
+        , "patient_id":row['patient_id']
+        , "study_id":row['study_id']
+        , "patient_age":row['patient_age']
+        , "psa":row['psa']
+        , "psad":row['psad']
+        , "prostate_volume":row['prostate_volume']
+        , "histopath_type":row['histopath_type']
+        , "lesion_GS":row['lesion_GS']
+        , "label":str(row['reSampledPath'])
+        
+        
+        }
+
+        return subject
+
+
+def load_df_only_full():
+    df = pd.read_csv('/home/sliceruser/labels/processedMetaData.csv')
+    df = df.loc[df['isAnyMissing'] ==False]
+    df = df.loc[df['isAnythingInAnnotated']>0 ]
+    deficientPatIDs=[]
+    data_dicts = list(map(lambda row: getMonaiSubjectDataFromDataFrame(row[1])  , list(df.iterrows())))
+    train_transforms=transformsForMain.get_train_transforms()
+    val_transforms= transformsForMain.get_val_transforms()
+
+    for dictt in data_dicts:    
+        try:
+            dat = train_transforms(dictt)
+            dat = val_transforms(dictt)
+        except:
+            deficientPatIDs.append(dictt['patient_id'])
+            print(dictt['patient_id'])
+
+
+    def isInDeficienList(row):
+            return row['patient_id'] not in deficientPatIDs
+
+    df["areTransformsNotDeficient"]= df.apply(lambda row : isInDeficienList(row), axis = 1)  
+
+    df = df.loc[ df['areTransformsNotDeficient']]
+
+    return df
---
+++ preprocessing/transformsForMain.py
@@ -0,0 +1,80 @@
+import monai
+from monai.transforms import (
+    EnsureChannelFirstd,
+    Orientationd,
+    AsDiscrete,
+    AddChanneld,
+    Spacingd,
+    Compose,
+    CropForegroundd,
+    LoadImaged,
+    Orientationd,
+    RandCropByPosNegLabeld,
+    ScaleIntensityRanged,
+    Spacingd,
+    EnsureTyped,
+    EnsureType,
+    Resize,
+    Resized,
+    RandSpatialCropd,
+        AsDiscrete,
+    AsDiscreted,
+    CropForegroundd,
+    LoadImaged,
+    Orientationd,
+    RandCropByPosNegLabeld,
+    SaveImaged,
+    ScaleIntensityRanged,
+
+    Invertd,
+)
+
+
+def get_train_transforms():
+    train_transforms = Compose(
+        [
+            LoadImaged(keys=["t2w", "label"]),
+            EnsureChannelFirstd(keys=["t2w", "label"]),
+            Orientationd(keys=["t2w", "label"], axcodes="RAS"),
+            Spacingd(keys=["t2w", "label"], pixdim=(
+                1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
+            tio.transforms.EnsureShapeMultiple((32 , 32, 32), include=["t2w", "label"]),
+            #CropForegroundd(keys=["image", "label"], source_key="image"),
+            RandCropByPosNegLabeld(
+                keys=["t2w", "label"],
+                label_key="label",
+                spatial_size=(32, 32, 32),
+                pos=1,
+                neg=1,
+                num_samples=4,
+                image_key="t2w",
+                image_threshold=0,
+            ),
+            EnsureTyped(keys=["t2w", "label"]),
+        ]
+    )
+    return train_transforms
+def get_val_transforms():
+    val_transforms = Compose(
+        [
+            LoadImaged(keys=["t2w", "label"]),
+            EnsureChannelFirstd(keys=["t2w", "label"]),
+            Orientationd(keys=["t2w", "label"], axcodes="RAS"),
+            Spacingd(keys=["t2w", "label"], pixdim=(
+                1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
+            tio.transforms.EnsureShapeMultiple((32 , 32, 32), include=["t2w", "label"]),
+            #CropForegroundd(keys=["image", "label"], source_key="image"),
+            # RandCropByPosNegLabeld(
+            #     keys=["t2w", "label"],
+            #     label_key="label",
+            #     spatial_size=(32, 32, 32),
+            #     pos=1,
+            #     neg=1,
+            #     num_samples=4,
+            #     image_key="t2w",
+            #     image_threshold=0,
+            # ),
+            EnsureTyped(keys=["t2w", "label"]),
+        ]
+    )
+    return val_transforms
