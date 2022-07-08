43d06bbfd8d3c4499323a9d0c4b4a2b01d45f4df
Jakub Mitura <jakub.mitura14@gmail.com>
2022-07-08 21:19:38 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- model/DataModule.py
+++ model/DataModule.py
@@ -130,6 +130,7 @@ class PiCaiDataModule(pl.LightningDataModule):
 
         self.dice_metric = DiceMetric(include_background=False, reduction="mean", get_not_nans=False)
 
+    #TODO replace with https://docs.monai.io/en/stable/data.html
     def splitDataSet(self,patList, trainSizePercent,noTestSet):
         """
         test train validation split
--- preprocessing/testPreprocess.py
+++ preprocessing/testPreprocess.py
@@ -47,10 +47,10 @@ for keyWord in ['t2w','adc', 'cor','hbv','sag'  ]:
 # get only complete representaions and only those with labels
 df = df.loc[df['isAnyMissing'] ==False]
 df = df.loc[df['isAnythingInAnnotated']>0 ]    
-print(df)    
 #just for testing    
-#df= df.head(4)
+df= df.head(8)
 ##df.to_csv('/home/sliceruser/data/metadata/processedMetaData_current.csv') 
+print(df)    
 
 ########## Standarization
 
@@ -104,6 +104,10 @@ def resample_ToMedianSpac(row,colName,targetSpacing,spacing_keyword):
 
 
 def resample_labels(row,targetSpacing,spacing_keyword):
+    """
+    performs labels resampling  to the target 
+    """
+
     path_t2w=row['t2w']
     path= path_t2w.replace(".mha","_stand_label.mha")
     
@@ -128,30 +132,17 @@ def resample_labels(row,targetSpacing,spacing_keyword):
     return newPath  
 
 
-def ifShortReturnMinus(tupl, patId,colName):
-    if(len(tupl)!=3):
-        print("incorrect spacial data "+ colName+ "  "+patId+ " length "+ len(tupl) ) 
-        return (-1,-1,-1)
-    return tupl 
-
-def get_spatial_meta(row,colName):
-    row=row[1]
-    patId=str(row['patient_id'])
-    path=str(row[colName])
-    if(len(path)>1):
-        image = sitk.ReadImage(path)
-        sizz= ifShortReturnMinus(image.GetSize(),patId,colName )
-        spac= ifShortReturnMinus(image.GetSpacing(),patId,colName)
-        orig= ifShortReturnMinus(image.GetOrigin(),patId,colName)
-        return list(sizz)+list(spac)+list(orig)
-    return [-1,-1,-1,-1,-1,-1,-1,-1,-1]
-
-
 def join_and_save_3Channel(row,colNameT2w,colNameAdc,colNameHbv,outPath):
     """
     join 3 images into 1 3 channel image
     """
     row=row[1]
+    print("debug")
+    print(row)
+    print(str(row[colNameT2w]))
+    print(str(row[colNameAdc]))
+    print(str(row[colNameHbv]))
+
     #patId=str(row['patient_id'])
     imgT2w=sitk.ReadImage(str(row[colNameT2w]))
     imgAdc=sitk.ReadImage(str(row[colNameAdc]))
@@ -164,32 +155,12 @@ def join_and_save_3Channel(row,colNameT2w,colNameAdc,colNameHbv,outPath):
     writer.Execute(joined_image)      
 
 
+#### 
+
+
 def preprocess_diffrent_spacings(df,targetSpacingg,spacing_keyword):
     print(f"**************    target spacing    ***************  {targetSpacingg}   {spacing_keyword}")  
-    ## bias field correction
-    Standardize.iterateAndBiasCorrect('t2w',df)
-    ## Standarization
-    for keyWord in ['t2w','adc', 'cor','hbv','sag'  ]:
-        ## denoising
-        Standardize.iterateAndDenoise(keyWord,df)
-        ## standarization
-        Standardize.iterateAndStandardize(keyWord,df,trainedModelsBasicPath,50)   
-    #standardize labels
-    Standardize.iterateAndchangeLabelToOnes(df)
-
-
-
-    resList=[]
     t2wKeyWord ="t2w"+spacing_keyword    
-
-    with mp.Pool(processes = mp.cpu_count()) as pool:
-        resList=pool.map(partial(get_spatial_meta,colName=keyWord)  ,list(df.iterrows()))    
-    print(type(resList))    
-    df[t2wKeyWord+'_sizz_x']= list(map(lambda arr:arr[0], resList))    
-    df[t2wKeyWord+'_sizz_y']= list(map(lambda arr:arr[1], resList))    
-    df[t2wKeyWord+'_sizz_z']= list(map(lambda arr:arr[2], resList))
-
-
     #needs to be on single thread as resampling GAN is acting on GPU
     # we save the metadata to main pandas data frame 
     df["adc"+spacing_keyword]=df.apply(lambda row : resample_ToMedianSpac(row, 'adc',targetSpacingg,spacing_keyword)   , axis = 1) 
@@ -203,37 +174,47 @@ def preprocess_diffrent_spacings(df,targetSpacingg,spacing_keyword):
         resList=[]     
         # list(map(partial(reg_adc_hbv_to_t2w,colName=keyWord,elacticPath=elacticPath,reg_prop=reg_prop,t2wColName='t2w_med_spac',experiment=experiment ),list(df.iterrows())))  
         with mp.Pool(processes = mp.cpu_count()) as pool:
-            resList=pool.map(partial(reg_adc_hbv_to_t2w,colName=keyWord,elacticPath=elacticPath,reg_prop=reg_prop,t2wColName='t2w_med_spac'),list(df.iterrows()))    
-            df['registered_'+keyWord]=resList  
+            resList=pool.map(partial(reg_adc_hbv_to_t2w,colName=keyWord,elacticPath=elacticPath,reg_prop=reg_prop,t2wColName=t2wKeyWord),list(df.iterrows()))    
+        df['registered_'+keyWord]=resList  
 
     ManageMetadata.addSizeMetaDataToDf(t2wKeyWord,df)
 
     ######Now we need to retrieve the maximum dimensions of resampled images
 
-
     #getting maximum size - so one can pad to uniform size if needed (for example in validetion test set)
-    median_spac_max_size_x = np.max(list(filter(lambda it: it>0 ,df[t2wKeyWord+"_sizz_x"].to_numpy() )))
-    median_spac_max_size_y = np.max(list(filter(lambda it: it>0 ,df[t2wKeyWord+"_sizz_y"].to_numpy() )))
-    median_spac_max_size_z = np.max(list(filter(lambda it: it>0 ,df[t2wKeyWord+"_sizz_z"].to_numpy() )))
-    maxSize = (median_spac_max_size_x,median_spac_max_size_y,median_spac_max_size_z  )
+    maxSize = ManageMetadata.getMaxSize(t2wKeyWord,df)
 
     print(f" max sizee {maxSize}")
-
-
     ###now we join it into 3 channel image
-    # resList=[]
-    # outPath = t2wKeyWord.replace('.mha', '_3Chan.mha')
+    resList=[]
+    outPath = t2wKeyWord.replace('.mha', '_3Chan.mha')
 
-    # with mp.Pool(processes = mp.cpu_count()) as pool:
-    #     resList=pool.map(partial(join_and_save_3Channel
-    #                             ,t2wKeyWord+spacing_keyword
-    #                             ,colNameAdc='registered_'+'adc'+spacing_keyword
-    #                             ,colNameHbv='registered_'+'hbv'+spacing_keyword
-    #                             ,outPath=outPath
-    #                             )  ,list(df.iterrows())) 
-    # df['registered_'+keyWord+"_3Chan"]=resList
+    with mp.Pool(processes = mp.cpu_count()) as pool:
+        resList=pool.map(partial(join_and_save_3Channel
+                                ,colNameT2w=t2wKeyWord
+                                ,colNameAdc='registered_'+'adc'+spacing_keyword
+                                ,colNameHbv='registered_'+'hbv'+spacing_keyword
+                                ,outPath=outPath
+                                )  ,list(df.iterrows())) 
+    df['registered_'+t2wKeyWord+"_3Chan"]=resList
 
-##### 
+
+## some preprocessing common for all
+## bias field correction
+
+# Standardize.iterateAndBiasCorrect('t2w',df)
+# ## Standarization
+# for keyWord in ['t2w','adc', 'cor','hbv','sag'  ]:
+#     ## denoising
+#     Standardize.iterateAndDenoise(keyWord,df)
+#     ## standarization
+#     Standardize.iterateAndStandardize(keyWord,df,trainedModelsBasicPath,50)   
+# #standardize labels
+# Standardize.iterateAndchangeLabelToOnes(df)
+
+
+#####
+ 
 targetSpacinggg=(spacingDict['t2w_spac_x'][3],spacingDict['t2w_spac_y'][3],spacingDict['t2w_spac_z'][3])
 preprocess_diffrent_spacings(df,targetSpacinggg,"_med_spac")
 preprocess_diffrent_spacings(df,(1.0,1.0,1.0),"_one_spac")
