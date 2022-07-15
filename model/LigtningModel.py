5cd93d52729eb2838dcccd1fe29b553b20968752
Jakub Mitura <53857487+jakubMitura14@users.noreply.github.com>
2022-07-15 12:29:32 +0200

    up

--- model/LigtningModel.py
+++ model/LigtningModel.py
@@ -119,11 +119,11 @@ class Model(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         y_hat, y = self.infer_batch(batch)
-        labelsb = [self.post_pred(i) for i in decollate_batch(y)]
-        concatLabels= torch.stack(labelsb)
+        y = torch.stack([self.post_pred(i) for i in decollate_batch(y)])
+        #concatLabels= torch.stack(labelsb)
         #print(f"labels {labelsb[0].size()}  labels type {type(labelsb[0])} concatLabels {  concatLabels.size()}  ")
 
-        loss = self.criterion(y_hat.to(device='cuda'), concatLabels.to(device='cuda'))
+        loss = self.criterion(y_hat.to(device='cuda'), y.to(device='cuda'))
 
         #loss = self.criterion(y_hat, y)
         self.log('train_loss', loss, prog_bar=True)
