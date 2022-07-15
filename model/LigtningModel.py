96dd883dc2a213d3d876f3d7e0eea0753d98ec37
Jakub Mitura <53857487+jakubMitura14@users.noreply.github.com>
2022-07-15 19:14:05 +0200

    up

--- model/LigtningModel.py
+++ model/LigtningModel.py
@@ -93,7 +93,9 @@ class Model(pl.LightningModule):
         self.dice_metric = DiceMetric(include_background=False, reduction="mean", get_not_nans=False)
         self.experiment=experiment
         self.picaiLossArr=[]
-        self.post_pred = Compose([ AsDiscrete(argmax=True, to_onehot=2)])
+        self.post_pred = Compose([ AsDiscrete( to_onehot=2)])
+#        self.post_pred = Compose([ AsDiscrete(argmax=True, to_onehot=2)])
+
         #self.post_label = Compose([EnsureType("tensor", device="cpu"), AsDiscrete(to_onehot=2)])
         #self.post_label = Compose([EnsureType("tensor", device="cpu"), torchio.transforms.OneHot(include=["label"] ,num_classes=2)])
         #self.post_label = Compose([EnsureType("tensor", device="cpu"), torchio.transforms.OneHot(include=["label"] ,num_classes=2)])
