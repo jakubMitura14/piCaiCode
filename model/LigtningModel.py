8bd8454da38d84c2578a5c76cda1ab806b2c53ae
Jakub Mitura <jakub.mitura14@gmail.com>
2022-07-16 20:10:46 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- model/LigtningModel.py
+++ model/LigtningModel.py
@@ -123,15 +123,13 @@ class Model(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         y_hat, y = self.infer_batch(batch)
         #print(f"aa  y_hat {y_hat.size()}  y  {y.size()}")  
-        y = torch.stack([self.post_pred(i) for i in decollate_batch(y)])
+        #y = torch.stack([self.post_pred(i) for i in decollate_batch(y)])
         #concatLabels= torch.stack(labelsb)
         #print(f"bb  cat y  {y.size()}")  
         
         #print(f"labels {labelsb[0].size()}  labels type {type(labelsb[0])} concatLabels {  concatLabels.size()}  ")
         loss = self.criterion(y_hat, y)
 
-
-        loss = self.criterion(y_hat, y)
         self.log('train_loss', loss, prog_bar=True)
         return loss
     # def validation_step(self, batch, batch_idx):
