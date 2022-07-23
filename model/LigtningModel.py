2b3e7de7c9d6ea1ddf757673310274183f90a574
Jakub Mitura <jakub.mitura14@gmail.com>
2022-07-23 11:05:15 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- model/LigtningModel.py
+++ model/LigtningModel.py
@@ -167,10 +167,10 @@ class Model(pl.LightningModule):
         #print(f"sss d y_hat {y_hat[0].size()} labels {labels[0].size()}  labels type {type(labels[0])} y_hat type {type(y_hat[0])}   ")
 
         #print(f" labels sum {torch.sum(labels)} ")
-        yNp=y_hat.cpu().detach().numpy()
+        y_det=iter(np.concatenate([x.cpu().detach().numpy() for x in y_hat], axis=0))
         
-        if not np.isnan(np.sum(yNp)):
-            valid_metrics = evaluate(y_det=iter(np.concatenate([x for x in yNp], axis=0)),
+        if not np.isnan(np.sum(y_det)):
+            valid_metrics = evaluate(y_det=y_det,
                                 y_true=iter(np.concatenate([x.cpu().detach().numpy() for x in labels], axis=0)),
                                 y_det_postprocess_func=lambda pred: extract_lesion_candidates(pred)[0])
 
