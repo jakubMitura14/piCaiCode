d708f7c0a38093f378aa39cdfac512e5e256a3ee
Jakub Mitura <jakub.mitura14@gmail.com>
2022-07-24 18:45:42 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- model/LigtningModel.py
+++ model/LigtningModel.py
@@ -181,7 +181,7 @@ class Model(pl.LightningModule):
 
         y_hat = decollate_batch(y_hat)
         labels = decollate_batch(labels)
-        print(f"y hat arg maxxed sum {torch.sum(np.argmax(y_hat[0].cpu().detach().numpy(),axis=0) )}   ")
+        print(f"y hat arg maxxed sum {np.sum(np.argmax(y_hat[0].cpu().detach().numpy(),axis=0) )}   ")
 
         # y_hat = decollate_batch(decollate_batch(y_hat)[0])
         # labels = decollate_batch(decollate_batch(labels)[0])
