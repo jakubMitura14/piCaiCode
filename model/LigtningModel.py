789fd2fc20b9c0f8eadc6b030aee2db246f113ca
Jakub Mitura <jakub.mitura14@gmail.com>
2022-07-25 09:41:09 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- model/LigtningModel.py
+++ model/LigtningModel.py
@@ -330,6 +330,7 @@ class Model(pl.LightningModule):
             # meanPiecaiMetr_AP= getMeanIgnoreNan(self.picaiLossArr_AP) # mean(self.picaiLossArr_AP)        
             # meanPiecaiMetr_score= getMeanIgnoreNan(self.picaiLossArr_score) #mean(self.picaiLossArr_score)        
 
+            print(f"meanPiecaiMetr_auroc {meanPiecaiMetr_auroc} meanPiecaiMetr_AP {meanPiecaiMetr_AP}  meanPiecaiMetr_score {meanPiecaiMetr_score} "  )
 
             self.log('val_mean_auroc', meanPiecaiMetr_auroc)
             self.log('val_mean_AP', meanPiecaiMetr_AP)
