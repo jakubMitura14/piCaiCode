f130e151eb7cbb96a3006d0414688dfd45954dfb
Jakub Mitura <jakub.mitura14@gmail.com>
2022-07-25 11:19:55 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- model/LigtningModel.py
+++ model/LigtningModel.py
@@ -79,10 +79,11 @@ import torchio
 
 torch.autograd.set_detect_anomaly(True)
 
-def getMeanIgnoreNan(a):
-    b=list(filter(lambda it: not np.isnan(it),a))
-    # b = a[np.logical_not(np.isnan(a))]
-    return np.mean(b)
+def divide_chunks(l, n):
+     
+    # looping till length l
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
 
 def saveFilesInDir(gold_arr,y_hat_arr, directory, patId):
     """
@@ -182,48 +183,18 @@ class Model(pl.LightningModule):
         primLabelsSum= torch.sum(labels)
 
         y_hat = sliding_window_inference(images, (32,32,32), 1, self.net)
-        #y_hat = self.net(images)
-        #print(f"sss y_hat {y_hat.size()} labels {labels.size()} labels type {type(labels)} y_hat type {type(y_hat)}   ")
-        #print(f"sss a y_hat {y_hat.size()} labels {labels.size()} labels type {type(labels)} y_hat type {type(y_hat)}   ")
-        # labelsb = [self.post_pred(i) for i in decollate_batch(labels)]
-        # concatLabels= torch.stack(labelsb)
-        #print(f"labels {labelsb[0].size()}  labels type {type(labelsb[0])} concatLabels {  concatLabels.size()}  ")
-        #print(f" uniqqqque y_hat {torch.unique(y_hat)} y  {torch.unique(labels)}  ")
+
         loss = self.criterion(y_hat, labels)
 
-        #labels= torch.nn.functional.one_hot(labels, num_classes=2) 
-        #y_hat = [(i) for i in decollate_batch(y_hat)]
-
-        #print(f"sss b  labels type {type(labels)} y_hat type {type(y_hat)}   ")
-        #print(f"sss b y_hat {y_hat.size()} labels {labels.size()} labels type {type(labels)} y_hat type {type(y_hat)}   ")
-        
-        #labelsb = [torch.nn.functional.one_hot(i.to(torch.int64), num_classes=- 1) for i in decollate_batch(labels)]
-        #print(f"sss c  labels type {type(labels)} y_hat type {type(y_hat)}   ")
-
-        #print(f"sss c y_hat {y_hat.size()} labels {labels.size()} labels type {type(labels)} y_hat type {type(y_hat)}   ")
-        #print(f"sss d y_hat {y_hat[0].size()} labels {labels[0].size()}  labels type {type(labels[0])} y_hat type {type(y_hat[0])}   ")
-
-        #print(f" labels sum {torch.sum(labels)} ")
-
-        #print(f"before decollate y_hat {y_hat.size()} labels{labels.size()}")
         y_hat=torch.sigmoid(y_hat)
         # print( f"before extract lesion  sum a {torch.sum(y_hat)  } " )
 
         y_det = decollate_batch(y_hat)
         y_true = decollate_batch(labels)
         patIds = decollate_batch(patIds)
-        # print(f"y hat arg maxxed sum {np.sum(np.argmax(y_hat[0].cpu().detach().numpy(),axis=0) )}  zzero {np.sum(y_hat[0].cpu().detach().numpy()[0,:,:,:])} first {np.sum(y_hat[0].cpu().detach().numpy()[0,:,:,:])}      ")
-        # print(f"y true arg maxxed sum {np.sum(np.argmax(labels[0].cpu().detach().numpy(),axis=0) )}  zzero {np.sum(labels[0].cpu().detach().numpy()[0,:,:,:])} first {np.sum(labels[0].cpu().detach().numpy()[0,:,:,:])}      ")
-
-        # y_hat = decollate_batch(decollate_batch(y_hat)[0])
-        # labels = decollate_batch(decollate_batch(labels)[0])
-
         #print(f"after decollate  y_hat{y_hat[0].size()} labels{labels[0].size()} y_hat len {len(y_hat)} labels len {len(labels)}")
         y_det=[extract_lesion_candidates( x.cpu().detach().numpy()[1,:,:,:])[0] for x in y_det]
         y_true=[x.cpu().detach().numpy()[1,:,:,:] for x in y_true]
-
-        # y_det=[extract_lesion_candidates( np.argmax(x.cpu().detach().numpy(),axis=0) )[0] for x in y_det]
-        # y_true=[x.cpu().detach().numpy()[1,:,:,:] for x in y_true]
 
 
         for i in range(0,len(y_true)):
@@ -233,68 +204,6 @@ class Model(pl.LightningModule):
         #now we need to save files in temporary direcory and save outputs to the appripriate lists wit paths
         
 
-
-
-
-
-
-        # print( f"suums y_det {np.sum(y_det[0])} y_true  {np.sum(y_true[0])} len { len(y_det) } shapes  y_det {np.shape(y_det[0])} y_true  {np.shape(y_true[0])} ")
-
-        # #print(f"single case {evaluate_case(y_hat[0], labels[0])}")
-
-
-        # # zipped=zip(y_hat,labels)
-        # # nonZeroHat= len(list(filter(lambda tupl : (torch.sum(tupl[0]).item()>0 )  ,zipped)))
-        # # nonZeroLab= len(list(filter(lambda tupl : (torch.sum(tupl[1]).item()>0 )  ,zipped)))
- 
-
-        # # zipped= list(filter(lambda tupl : (torch.sum(tupl[0].item())>0 and torch.sum(tupl[1].item())>0   )  ,zipped))
-        # # y_hat= list(map(lambda tupl : tupl[0], zipped))
-        # # labels= list(map(lambda tupl : tupl[1], zipped))
-
-
-        # #print(f" zipped len {len(zipped)} nonZeroHat {nonZeroHat}  nonZeroLab {nonZeroLab} primLabelsSum {primLabelsSum}")
-        # #if(len(zipped)>0 ):
-        # if(True):
-        #     # y_det=iter(np.concatenate(y_det, axis=0))
-        #     # y_true=iter(np.concatenate(y_true, axis=0))
-
-        #     # y_det=np.concatenate([x.cpu().detach().numpy() for x in y_hat], axis=0)
-        #     # y_true=np.concatenate([x.cpu().detach().numpy() for x in labels], axis=0)
-
-
-
-        # #print(f"validation images {torch.sum(torch.isnan(images))} label {torch.sum(torch.isnan(labels))} y_hat {np.sum(y_det)} loss {loss}"  )
-        #     valid_metrics = evaluate(y_det=y_det,
-        #                         y_true=y_true,
-        #                         #y_true=iter(y_true),
-        #                         #y_det_postprocess_func=lambda pred: extract_lesion_candidates(pred)[0]
-        #                         )
-
-        # # for i in range(0, len(labelsb)):
-        # #     metrics = evaluate(
-        # #         y_det=y_hat[i].cpu().detach().numpy(),
-        # #         y_true=labelsb[i].cpu().detach().numpy(),
-        # #     )
-        #     self.picaiLossArr_auroc.append(valid_metrics.auroc)
-        #     self.picaiLossArr_AP.append(valid_metrics.AP  )
-        #     self.picaiLossArr_score.append(valid_metrics.score)
-            
-            
-        #     # meanPiecaiMetr_auroc= mean(self.picaiLossArr_auroc)
-        #     # meanPiecaiMetr_AP= mean(self.picaiLossArr_AP)        
-        #     # meanPiecaiMetr_score= mean(self.picaiLossArr_score)  
-
-        #     meanPiecaiMetr_auroc= valid_metrics.auroc
-        #     meanPiecaiMetr_AP= valid_metrics.AP   
-        #     meanPiecaiMetr_score= valid_metrics.score
-            
-            
-        #     print( f"metrics.auroc {meanPiecaiMetr_auroc} metrics.AP {meanPiecaiMetr_AP}  metrics.score {meanPiecaiMetr_score}  " )
-
-
-        #self.dice_metric(y_pred=y_hat, y=labels)
-        # print(f"losss {loss}  ")
         self.log('val_loss', loss)
 
         return loss
@@ -306,27 +215,39 @@ class Model(pl.LightningModule):
         """
         just in order to log the dice metric on validation data 
         """
-        # mean_val_dice = self.dice_metric.aggregate().item()
-        # self.dice_metric.reset()
-        # if mean_val_dice > self.best_val_dice:
-        #     self.best_val_dice = mean_val_dice
-        #     self.best_val_epoch = self.current_epoch
-        # print(
-        #     f"current epoch: {self.current_epoch} "
-        #     f"current mean dice: {mean_val_dice:.4f}"
-        #     f"\nbest mean dice: {self.best_val_dice:.4f} "
-        #     f"at epoch: {self.best_val_epoch}"
-        # )
+        chunkLen=8
+        chunksNumb=math.floor(len(self.list_yHat_val)/chunkLen)
         if(len(self.list_yHat_val)>0):
-            valid_metrics = evaluate(y_det=list(map(getArrayFromPath, self.list_yHat_val)),
-                                y_true=list(map(getArrayFromPath, self.list_gold_val  )),
-                                #y_true=iter(y_true),
-                                #y_det_postprocess_func=lambda pred: extract_lesion_candidates(pred)[0]
-                                )
+            for i in range(0,chunksNumb):
+                startIndex= i*chunkLen
+                endIndex=(i+1)*chunkLen
+                valid_metrics = evaluate(y_det=list(map(getArrayFromPath, self.list_yHat_val[startIndex:endIndex])),
+                                    y_true=list(map(getArrayFromPath, self.list_gold_val[startIndex:endIndex]  )),
+                                    #y_true=iter(y_true),
+                                    #y_det_postprocess_func=lambda pred: extract_lesion_candidates(pred)[0]
+                                    )
+                self.picaiLossArr_auroc.append(valid_metrics.auroc)
+                self.picaiLossArr_AP.append(valid_metrics.AP)
+                self.picaiLossArr_score.append(valid_metrics.score)
+            
+            
+            startIndex= chunksNumb*chunkLen
+            endIndex=len(self.list_yHat_val)-1
+            # and the last part
+            valid_metrics = evaluate(y_det=list(map(getArrayFromPath, self.list_yHat_val[startIndex:endIndex])),
+                                    y_true=list(map(getArrayFromPath, self.list_gold_val[startIndex:endIndex]  )),
+                                    #y_true=iter(y_true),
+                                    #y_det_postprocess_func=lambda pred: extract_lesion_candidates(pred)[0]
+                                    )
+            self.picaiLossArr_auroc.append(valid_metrics.auroc)
+            self.picaiLossArr_AP.append(valid_metrics.AP)
+            self.picaiLossArr_score.append(valid_metrics.score)
 
-            meanPiecaiMetr_auroc= valid_metrics.auroc
-            meanPiecaiMetr_AP=valid_metrics.AP 
-            meanPiecaiMetr_score=valid_metrics.score
+
+
+            meanPiecaiMetr_auroc= np.nanmean(self.picaiLossArr_auroc) 
+            meanPiecaiMetr_AP=np.nanmean(self.picaiLossArr_AP) 
+            meanPiecaiMetr_score=np.nanmean(self.picaiLossArr_score) 
             
 
         
@@ -350,9 +271,9 @@ class Model(pl.LightningModule):
             self.picaiLossArr_score_final.append(meanPiecaiMetr_score)
 
             #resetting to 0 
-            # self.picaiLossArr_auroc=[]
-            # self.picaiLossArr_AP=[]
-            # self.picaiLossArr_score=[]
+            self.picaiLossArr_auroc=[]
+            self.picaiLossArr_AP=[]
+            self.picaiLossArr_score=[]
 
 
             #clearing and recreatin temporary directory
