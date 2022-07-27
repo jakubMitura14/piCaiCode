823637e4acc2632b601b6c5ac74c2e30385b1ee6
Jakub Mitura <jakub.mitura14@gmail.com>
2022-07-27 17:18:55 +0200

    up optimizer id
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- Three_chan_baseline_hyperParam.py
+++ Three_chan_baseline_hyperParam.py
@@ -159,10 +159,8 @@ df= manageMetaData.load_df_only_full(
 # (You can leave out API_KEY if you already set it)
 #opt = Optimizer(config)
 
-#opt = Optimizer("6f64b92b125c4e189c33a6ae3a718d11", api_key="yB0irIjdk9t7gbpTlSUPnXBd4")
-opt = Optimizer(config, api_key="yB0irIjdk9t7gbpTlSUPnXBd4")
-
-
+opt = Optimizer("7e7ecc7b196e4a9da030d51296622454", api_key="yB0irIjdk9t7gbpTlSUPnXBd4")
+#opt = Optimizer(config, api_key="yB0irIjdk9t7gbpTlSUPnXBd4")
 # print("zzzzzzzzz")
 #  print(opt.get_experiments(
 #          api_key="yB0irIjdk9t7gbpTlSUPnXBd4",
