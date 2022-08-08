80530b2448a3246f37a49a5158a38bafbbc0818a
Jakub Mitura <jakub.mitura14@gmail.com>
2022-08-08 21:04:35 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- preprocessing/testPreprocess.py
+++ preprocessing/testPreprocess.py
@@ -341,15 +341,15 @@ def preprocess_diffrent_spacings(df,targetSpacingg,spacing_keyword):
 
 
 #bias field correction
-#Standardize.iterateAndBiasCorrect('t2w',df)
+Standardize.iterateAndBiasCorrect('t2w',df)
 #Standarization
-# for keyWord in ['t2w','adc', 'hbv']: #'cor',,'sag'
-#     ## denoising
-#     #Standardize.iterateAndDenoise(keyWord,df)
-#     ## standarization
-#     Standardize.iterateAndStandardize(keyWord,df,trainedModelsBasicPath,50)   
-# #standardize labels
-# Standardize.iterateAndchangeLabelToOnes(df)
+for keyWord in ['t2w','adc', 'hbv']: #'cor',,'sag'
+    ## denoising
+    #Standardize.iterateAndDenoise(keyWord,df)
+    ## standarization
+    Standardize.iterateAndStandardize(keyWord,df,trainedModelsBasicPath,80)   
+#standardize labels
+Standardize.iterateAndchangeLabelToOnes(df)
 
 #### 
 #first get adc and tbv to t2w spacing
