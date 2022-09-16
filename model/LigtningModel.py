b89e005cb5c95812cbcde48c02efa497e806323e
Jakub Mitura <jakub.mitura14@gmail.com>
2022-09-16 14:05:21 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- model/LigtningModel.py
+++ model/LigtningModel.py
@@ -184,7 +184,6 @@ class Model(pl.LightningModule):
         # in case we have odd iteration we get access only to number of lesions present in the image not where they are (if they are present at all)    
         else:
             regress_res=self.modelRegression(y_hat)
-            print(f" numLesions {numLesions} type {type(numLesions)} regress_res {regress_res}")
             numLesions=list(map(lambda entry : int(entry), numLesions ))
             numLesions=torch.Tensor(numLesions).to(self.device)
             return F.smooth_l1_loss(regress_res, numLesions )
