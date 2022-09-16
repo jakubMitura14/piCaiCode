51615590fff75bc27f1f234d6528926156688ba2
Jakub Mitura <jakub.mitura14@gmail.com>
2022-09-16 17:39:40 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- ThreeChanNoExperiment.py
+++ ThreeChanNoExperiment.py
@@ -169,7 +169,7 @@ def train_model(label_name, dummyLabelPath, df,percentSplit,cacheDir
         accumulate_grad_batches=accumulate_grad_batches,
         gradient_clip_val=gradient_clip_val,# 0.5,2.0
         log_every_n_steps=30,
-        strategy='ddp' # for multi gpu training
+        strategy='ddp2' # for multi gpu training
     )
 
 
