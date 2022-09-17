78220fdfada3d63d835198858516e6ea7a6f8be5
Jakub Mitura <jakub.mitura14@gmail.com>
2022-09-17 16:58:48 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- model/DataModule.py
+++ model/DataModule.py
@@ -265,9 +265,9 @@ class PiCaiDataModule(pl.LightningDataModule):
 
 
         return {"all": DataLoader(self.train_ds_all, batch_size=self.batch_size, drop_last=self.drop_last
-                          ,num_workers=self.num_workers,collate_fn=list_data_collate)
+                          ,num_workers=self.num_workers)#,collate_fn=list_data_collate
         , "pos": DataLoader(self.train_ds_pos, batch_size=self.batch_size, drop_last=self.drop_last
-                          ,num_workers=self.num_workers,collate_fn=list_data_collate)}
+                          ,num_workers=self.num_workers)}#,collate_fn=list_data_collate
         # return DataLoader(self.train_ds, batch_size=self.batch_size, drop_last=self.drop_last
         #                   ,num_workers=self.num_workers,collate_fn=list_data_collate)#,collate_fn=list_data_collate , shuffle=True
 
