aaa9221a57bb7ccea542f6cfcc07406f74e880f8
Jakub Mitura <jakub.mitura14@gmail.com>
2022-09-17 10:13:02 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- Three_chan_baseline.py
+++ Three_chan_baseline.py
@@ -103,7 +103,7 @@ def mainTrain(experiment,options,df,experiment_name):
     dummyLabelPath='/home/sliceruser/data/dummyData/zeroLabel.nii.gz'
     sizz=semisuperPreprosess.writeDummyLabels(dummyLabelPath,imageRef_path)
     img_size = (sizz[2],sizz[1],sizz[0])
-
+    print(f"aaaaaa  img_size {img_size}  {type(img_size)}")
     RandGaussianNoised_prob=experiment.get_parameter("RandGaussianNoised_prob")
     RandAdjustContrastd_prob=experiment.get_parameter("RandAdjustContrastd_prob")
     RandGaussianSmoothd_prob=experiment.get_parameter("RandGaussianSmoothd_prob")
