0bee5040a7b07a6d63118765ce49676cbf15805b
Jakub Mitura <jakub.mitura14@gmail.com>
2022-09-18 20:36:50 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- model/LigtningModel.py
+++ model/LigtningModel.py
@@ -299,8 +299,9 @@ class Model(pl.LightningModule):
         regress_res3=list(map(lambda el:round(el) ,torch.flatten(regress_res2).cpu().detach().numpy() ))
         #print( f"torch.Tensor(numLesions).cpu() {torch.Tensor(numLesions).cpu()}  torch.Tensor(regress_res).cpu() {torch.Tensor(regress_res).cpu()}   ")
         total_loss=precision_recall(torch.Tensor(regress_res3).int(), torch.Tensor(numLesions2).cpu().int(), average='micro', num_classes=4)
-        print(f" total loss a {total_loss}")
-        total_loss2= torch.add(total_loss,dice.aggregate())
+        total_loss1=torch.mean(torch.stack(total_loss[0],total_loss[1] ))
+        print(f" total loss a {total_loss1}")
+        total_loss2= torch.add(total_loss1,dice.aggregate())
         print(f" total loss b {total_loss2}  total_loss,dice.aggregate() {dice.aggregate()}")
 
         #print(f"sd.aggregate() {sd.aggregate().item()}")
