52a7e863f6d22189372a4cf042f73c8da1481293
Jakub Mitura <jakub.mitura14@gmail.com>
2022-09-20 20:56:42 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- ThreeChanNoExperiment.py
+++ ThreeChanNoExperiment.py
@@ -199,6 +199,7 @@ def train_model(label_name, dummyLabelPath, df,percentSplit,cacheDir
     # )
     callbacks=[checkPointCallback]
     kwargs = {
+        "accelerator":'auto',
         "max_epochs": max_epochs,
         "callbacks" :callbacks,
         "logger" : comet_logger,
