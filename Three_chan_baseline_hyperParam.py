12832432701182de8c076962636ff43d45d440ec
Jakub Mitura <jakub.mitura14@gmail.com>
2022-09-22 10:24:37 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- Three_chan_baseline_hyperParam.py
+++ Three_chan_baseline_hyperParam.py
@@ -294,8 +294,8 @@ experiment_name="picai-hyperparam-search-30"
 # Three_chan_baseline.mainTrain(options,df,experiment_name,dummyDict)
 num_gpu=2
 cpu_num=8 #per gpu
-default_root_dir='/home/sliceruser/data/lightningB'
-checkpoint_dir='/home/sliceruser/data/tuneCheckpoints3'
+default_root_dir='/home/sliceruser/data/lightningC'
+checkpoint_dir='/home/sliceruser/data/tuneCheckpoints4'
 # os.makedirs(checkpoint_dir,  exist_ok = True) 
 # os.makedirs(default_root_dir,  exist_ok = True) 
 num_cpus_per_worker=cpu_num
