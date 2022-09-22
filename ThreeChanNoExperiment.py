e9008ce311037aec2fddf1b6e10a6ec326a1f63a
Jakub Mitura <jakub.mitura14@gmail.com>
2022-09-22 10:11:09 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- ThreeChanNoExperiment.py
+++ ThreeChanNoExperiment.py
@@ -175,8 +175,8 @@ def train_model(label_name, dummyLabelPath, df,percentSplit,cacheDir
     #     },
     #     on="validation_end")
 
-    #strategy = RayShardedStrategy(num_workers=1,  use_gpu=True)#num_cpus_per_worker=1, num_gpu
-    strategy = RayStrategy(num_workers=1, num_cpus_per_worker=num_cpus_per_worker, use_gpu=True)
+    strategy = RayShardedStrategy(num_workers=1,num_cpus_per_worker=num_cpus_per_worker,  use_gpu=True)#num_cpus_per_worker=1, num_gpu
+    #strategy = RayStrategy(num_workers=1, num_cpus_per_worker=num_cpus_per_worker, use_gpu=True)
 
 
     # trainer = pl.Trainer(
