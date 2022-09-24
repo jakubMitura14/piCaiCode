d6761cedf91d3679307c5b17a42220af10d0985a
Jakub Mitura <jakub.mitura14@gmail.com>
2022-09-24 15:08:12 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- exploration/rayPlay/rayExampleBasic.py
+++ exploration/rayPlay/rayExampleBasic.py
@@ -219,7 +219,7 @@ def tune_mnist(data_dir,
                num_samples=2,
                num_epochs=10,
                num_workers=2,
-               use_gpu=False):
+               use_gpu=True):
     config = {
         "layer_1": tune.choice([32, 64, 128]),
         "layer_2": tune.choice([64, 128, 256]),
