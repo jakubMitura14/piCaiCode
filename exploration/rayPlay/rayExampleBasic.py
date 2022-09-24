a288e1eaf9ed7389d3e4eedb26cd5573e8bb3640
Jakub Mitura <jakub.mitura14@gmail.com>
2022-09-24 20:04:19 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- exploration/rayPlay/rayExampleBasic.py
+++ exploration/rayPlay/rayExampleBasic.py
@@ -2,127 +2,15 @@
 import os
 import tempfile
 
-import time
-from pathlib import Path
-from datetime import datetime
-import SimpleITK as sitk
-from monai.utils import set_determinism
 import math
 import torch
-from torch.utils.data import random_split, DataLoader
-import monai
-import gdown
-import pandas as pd
-import torchio as tio
-import pytorch_lightning as pl
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-from sklearn.model_selection import train_test_split
-from monai.networks.nets import UNet
-from monai.networks.layers import Norm
-from monai.metrics import DiceMetric
-from monai.losses import DiceLoss
-from monai.inferers import sliding_window_inference
-from monai.data import CacheDataset,Dataset,PersistentDataset, list_data_collate, decollate_batch
-from monai.config import print_config
-from monai.apps import download_and_extract
 
 from datetime import datetime
 import os
 import tempfile
 from glob import glob
-from monai.handlers.utils import from_engine
-from monai.networks.nets import UNet
-from monai.networks.layers import Norm
-from monai.metrics import DiceMetric
-from monai.losses import DiceLoss
-from monai.inferers import sliding_window_inference
-from monai.config import print_config
-from monai.apps import download_and_extract
-import torch
-import matplotlib.pyplot as plt
-import tempfile
-import shutil
-import os
-import glob
-from picai_eval import evaluate
-#from picai_eval.picai_eval import evaluate_case
-from statistics import mean
-from report_guided_annotation import extract_lesion_candidates
-from scipy.ndimage import gaussian_filter
-import tempfile
-import shutil
-from os import path as pathOs
-from os.path import basename, dirname, exists, isdir, join, split
 import torch.nn as nn
 import torch.nn.functional as F
-
-from monai.metrics import (DiceMetric, HausdorffDistanceMetric,
-                           SurfaceDistanceMetric)
-from torchmetrics import Precision
-from monai.transforms import (
-    AsDiscrete,
-    AddChanneld,
-    Compose,
-    CropForegroundd,
-    LoadImaged,
-    Orientationd,
-    RandCropByPosNegLabeld,
-    ScaleIntensityRanged,
-    Spacingd,
-    EnsureTyped,
-    EnsureType,
-)
-import torchio
-import importlib.util
-import sys
-import warnings
-from typing import Optional, Sequence, Tuple, Union
-import torch
-import torch.nn as nn
-from monai.networks.blocks.convolutions import Convolution, ResidualUnit
-from monai.networks.layers.factories import Act, Norm
-from monai.networks.layers.simplelayers import SkipConnection
-from monai.utils import alias, deprecated_arg, export
-import functools
-import operator
-from torch.nn.intrinsic.qat import ConvBnReLU3d
-
-import multiprocessing as mp
-import time
-from functools import partial
-from torchmetrics.functional import precision_recall
-from torch.utils.cpp_extension import load
-import torchmetrics
-from pl_bolts.datamodules.mnist_datamodule import MNISTDataModule
-
-import pytorch_lightning as pl
-import ray
-from ray import tune
-from ray_lightning.tune import TuneReportCallback, get_tune_resources
-from ray_lightning import RayStrategy
-import os
-from typing import Optional, List
-
-import torch
-import torch.nn.functional as F
-from torch.utils.data import Dataset
-
-import pytorch_lightning as pl
-from pytorch_lightning.strategies import Strategy
-from pytorch_lightning import LightningModule, Callback, Trainer, \
-    LightningDataModule
-
-import torchmetrics
-import torch
-import torch.nn.functional as F
-from torch.utils.data import Dataset
-
-import pytorch_lightning as pl
-from pytorch_lightning.strategies import Strategy
-from pytorch_lightning import LightningModule, Callback, Trainer, \
-    LightningDataModule
 
 import torchmetrics
 from ray import air, tune
@@ -135,7 +23,7 @@ from ray_lightning import RayShardedStrategy
 
 ray.init(num_cpus=24)
 data_dir = '/home/sliceruser/mnist'
-#MNISTDataModule(data_dir=data_dir).prepare_data()
+MNISTDataModule(data_dir=data_dir).prepare_data()
 num_cpus_per_worker=6
 test_l_dir = '/home/sliceruser/test_l_dir'
 
@@ -163,10 +51,6 @@ class LightningMNISTClassifier(pl.LightningModule):
         self.lr = config["lr"]
         self.batch_size = config["batch_size"]
 
-        # mnist images are (1, 28, 28) (channels, width, height)
-        # self.layer_1 = torch.nn.Linear(28 * 28, layer_1)
-        # self.layer_2 = torch.nn.Linear(layer_1, layer_2)
-        # self.layer_3 = torch.nn.Linear(layer_2, 10)
         self.accuracy = torchmetrics.Accuracy()
         self.netA= netaA(config)
 
@@ -211,11 +95,6 @@ def train_mnist(config,
                 num_workers=1,
                 use_gpu=True,
                 callbacks=None):
-    # Make sure data is downloaded on all nodes.
-    # def download_data():
-    #     from filelock import FileLock
-    #     with FileLock(os.path.join(data_dir, ".lock")):
-    #         MNISTDataModule(data_dir=data_dir).prepare_data()
 
     model = LightningMNISTClassifier(config, data_dir)
 
@@ -246,7 +125,7 @@ def tune_mnist(data_dir,
 
     # Add Tune callback.
     metrics = {"loss": "ptl/val_loss", "acc": "ptl/val_accuracy"}
-    callbacks = [TuneReportCheckpointCallback(metrics, on="validation_end")]#,filename="checkpointtt"
+    callbacks = [TuneReportCheckpointCallback(metrics, on="validation_end",filename="checkpointtt")]
     trainable = tune.with_parameters(
         train_mnist,
         data_dir=data_dir,
