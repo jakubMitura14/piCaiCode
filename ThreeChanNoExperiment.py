e1d3550b47613a0a98219183f6dd78864596880e
Jakub Mitura <jakub.mitura14@gmail.com>
2022-09-27 20:33:45 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- ThreeChanNoExperiment.py
+++ ThreeChanNoExperiment.py
@@ -88,7 +88,7 @@ def train_model(label_name, dummyLabelPath, df,percentSplit,cacheDir
     
     data = DataModule.PiCaiDataModule(
         df= df,
-        batch_size=3,#
+        batch_size=5,#
         trainSizePercent=percentSplit,# 
         num_workers=cpu_num,#os.cpu_count(),
         drop_last=False,#True,
@@ -205,7 +205,7 @@ def train_model(label_name, dummyLabelPath, df,percentSplit,cacheDir
         "accumulate_grad_batches" : accumulate_grad_batches,
         "gradient_clip_val" :gradient_clip_val,
         "log_every_n_steps" :2,
-        "strategy" :'ddp_sharded'
+        #"strategy" :'ddp_sharded'
         }
 
     # if os.path.exists(os.path.join(checkpoint_dir, "checkpointtt")):
