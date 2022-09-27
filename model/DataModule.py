087897d53f58eb64808d1e73b5d16b329b546216
Jakub Mitura <jakub.mitura14@gmail.com>
2022-09-27 10:03:20 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- model/DataModule.py
+++ model/DataModule.py
@@ -96,7 +96,7 @@ def getMonaiSubjectDataFromDataFrame(row,chan3_col_name,label_name,chan3_col_nam
         #, "hbv":str(row[hbv_name])
         #, "sag":str(row['sag'])
         #, "t2w":str(row[t2w_name])
-        , "isAnythingInAnnotated":str(row['isAnythingInAnnotated'])
+        , "isAnythingInAnnotated":int(row['isAnythingInAnnotated'])
         , "patient_id":str(row['patient_id'])
         , "num_lesions_to_retain":str(row['num_lesions_to_retain'])
         # , "study_id":row['study_id']
@@ -217,18 +217,18 @@ class PiCaiDataModule(pl.LightningDataModule):
         set_determinism(seed=0)
         allSubj,onlyPositve=  self.getSubjects()
 
-        allSubjects= allSubj[400:500]
-        onlyPositiveSubjects= onlyPositve[0:20]
+        allSubjects= allSubj[400:440]
+        #onlyPositiveSubjects= onlyPositve[0:20]
         random.shuffle(allSubjects)
-        random.shuffle(onlyPositiveSubjects)
+        #random.shuffle(onlyPositiveSubjects)
 
 
         self.allSubjects= allSubjects
-        self.onlyPositiveSubjects=onlyPositiveSubjects
+        #self.onlyPositiveSubjects=onlyPositiveSubjects
 
         #print(f"self.allSubjects {len(self.allSubjects)}  self.onlyPositiveSubjects {len(self.onlyPositiveSubjects)}")
         train_set_all, valid_set_all,test_set_all = self.splitDataSet(self.allSubjects , self.trainSizePercent,True)
-        train_set_pos, valid_set_pos,test_set_pos = self.splitDataSet(self.onlyPositiveSubjects , self.trainSizePercent,True)
+        # train_set_pos, valid_set_pos,test_set_pos = self.splitDataSet(self.onlyPositiveSubjects , self.trainSizePercent,True)
         #so we get just the example where we should not detect a thing 
         onlyNegatives=list(filter(lambda subj :  subj['num_lesions_to_retain']==0  ,valid_set_all))
 
@@ -260,9 +260,9 @@ class PiCaiDataModule(pl.LightningDataModule):
 # 
 # PersistentDataset
         # self.train_ds_all =  CacheDataset(data=train_set_all, transform=train_transforms)
-        self.val_ds=     CacheDataset(data=valid_set_pos, transform=val_transforms)
+        self.val_ds=     CacheDataset(data=valid_set_all, transform=val_transforms)
         # self.val_ds=     CacheDataset(data=valid_set_pos+onlyNegatives[0: int(round(len(valid_set_pos)/2)) ], transform=val_transforms)
-        self.train_ds_pos =  CacheDataset(data=train_set_pos, transform=train_transforms)
+        self.train_ds_all =  CacheDataset(data=train_set_all, transform=train_transforms)
 
 
 
@@ -286,8 +286,8 @@ class PiCaiDataModule(pl.LightningDataModule):
         #                   ,num_workers=self.num_workers,sampler=RandomSampler(self.train_ds_all,num_samples=self.forTrainingSamplesNum))#,collate_fn=list_data_collate
         # , "pos": DataLoader(self.train_ds_pos, batch_size=self.batch_size, drop_last=self.drop_last
         #                   ,num_workers=self.num_workers,sampler=RandomSampler(self.train_ds_pos,num_samples=self.forTrainingSamplesNum))}#,collate_fn=list_data_collate
-        return {"pos": DataLoader(self.train_ds_pos, batch_size=self.batch_size, drop_last=self.drop_last
-                          ,num_workers=self.num_workers)}#,collate_fn=list_data_collate
+        return DataLoader(self.train_ds_all, batch_size=self.batch_size, drop_last=self.drop_last
+                          ,num_workers=self.num_workers)#,collate_fn=list_data_collate
         
         # return {"all": DataLoader(self.train_ds_all, batch_size=self.batch_size, drop_last=self.drop_last
         #                   ,num_workers=self.num_workers)#,collate_fn=list_data_collate
--- model/LigtningModel.py
+++ model/LigtningModel.py
@@ -95,22 +95,14 @@ from torch.utils.cpp_extension import load
 import torchmetrics
 # lltm_cuda = load('lltm_cuda', ['lltm_cuda.cpp', 'lltm_cuda_kernel.cu'], verbose=True)
 
-# def loadLib(name,path):
-#     spec = importlib.util.spec_from_file_location(name, path)
-#     res = importlib.util.module_from_spec(spec)
-#     sys.modules[name] = res
-#     spec.loader.exec_module(res)
-#     return res
-
-# detectSemiSupervised =loadLib("detectSemiSupervised", "/home/sliceruser/data/piCaiCode/model/detectSemiSupervised.py")
-
-
 class UNetToRegresion(nn.Module):
     def __init__(self,
         in_channels,
         regression_channels
+        ,segmModel
     ) -> None:
         super().__init__()
+        self.segmModel=segmModel
         self.model = nn.Sequential(
             ConvBnReLU3d(in_channels=in_channels, out_channels=regression_channels[0], kernel_size=3, stride=2,qconfig = torch.quantization.get_default_qconfig('fbgemm')),
             ConvBnReLU3d(in_channels=regression_channels[0], out_channels=regression_channels[1], kernel_size=3, stride=2,qconfig = torch.quantization.get_default_qconfig('fbgemm')),
@@ -125,7 +117,8 @@ class UNetToRegresion(nn.Module):
             nn.Linear(in_features=100, out_features=1)
         )
     def forward(self, x):
-        return self.model(x)
+        segmMap=self.segmModel(x)
+        return (segmMap,self.model(segmMap))
 
 
 
@@ -189,8 +182,7 @@ class Model(pl.LightningModule):
     ):
         super().__init__()
         self.lr = learning_rate
-        self.net = net
-        #self.modelRegression = UNetToRegresion(2,regression_channels)
+        self.modelRegression = UNetToRegresion(2,regression_channels,net)
         self.criterion = criterion
         self.optimizer_class = optimizer_class
         self.best_val_dice = 0
@@ -222,65 +214,68 @@ class Model(pl.LightningModule):
     
 
     
-    def infer_batch_pos(self, batch):
-        x, y, numLesions = batch["pos"]['chan3_col_name'], batch["pos"]['label'], batch["pos"]['num_lesions_to_retain']
-        y_hat = self.net(x)
-        return y_hat, y, numLesions
+    # def infer_batch_pos(self, batch):
+    #     x, y, numLesions = batch["pos"]['chan3_col_name'], batch["pos"]['label'], batch["pos"]['num_lesions_to_retain']
+    #     y_hat = self.net(x)
+    #     return y_hat, y, numLesions
 
 
 
-    def infer_batch_all(self, batch):
-        x, numLesions =batch["all"]['chan3_col_name'], batch["all"]['num_lesions_to_retain']
-        y_hat = self.net(x)
-        return y_hat, numLesions
+    # def infer_batch_all(self, batch):
+    #     x, numLesions =batch["all"]['chan3_col_name'], batch["all"]['num_lesions_to_retain']
+    #     y_hat = self.net(x)
+    #     return y_hat, numLesions
+
+    def calculateLoss(self,isAnythingInAnnotated,seg_hat,y_true,reg_hat,numLesions):
+        if(isAnythingInAnnotated>0):
+            lossSeg=self.criterion(seg_hat, y_true)
+            lossReg=F.smooth_l1_loss(reg_hat,numLesions)
+            return torch.add(lossSeg,lossReg)
+        return  F.smooth_l1_loss(reg_hat,numLesions) 
 
 
     def training_step(self, batch, batch_idx):
         # every second iteration we will do the training for segmentation
-        y_hat, y , numLesions_ab= self.infer_batch_pos(batch)
-        lossa = self.criterion(y_hat, y)
-        # regressab=  self.modelRegression(y_hat)
-        # numLesions_ab2=list(map(lambda entry : int(entry), numLesions_ab ))
-        # numLesions_ab3=torch.Tensor(numLesions_ab2).to(self.device)  
-        # lossab=F.smooth_l1_loss(torch.flatten(regressab), torch.flatten(numLesions_ab3) )
-      
-        # # in case we have odd iteration we get access only to number of lesions present in the image not where they are (if they are present at all)    
-        # y_hat_all, numLesions= self.infer_batch_all(batch)
-        # regress_res=self.modelRegression(y_hat_all)
-        # numLesions1=list(map(lambda entry : int(entry), numLesions ))
-        # numLesions2=torch.Tensor(numLesions1).to(self.device)
-        # # print(f" regress res {torch.flatten(regress_res).size()}  orig {torch.flatten(numLesions).size() } ")
-        # lossb=F.smooth_l1_loss(torch.flatten(regress_res), torch.flatten(numLesions2) )
+        x, y_true, numLesions,isAnythingInAnnotated = batch['chan3_col_name'], batch['label'], batch['num_lesions_to_retain'], batch['isAnythingInAnnotated']
+        seg_hat, reg_hat = self.modelRegression(x)
+        return self.calculateLoss(isAnythingInAnnotated,seg_hat,y_true,reg_hat,numLesions)
 
-        # self.log('train_loss', torch.add(lossa,lossb), prog_bar=True)
-        # self.log('train_image_loss', lossa, prog_bar=True)
-        # self.log('train_reg_loss', lossb, prog_bar=True)
-        # return torch.add(torch.add(lossa,lossb),lossab)
-        return lossa
+        # if(isAnythingInAnnotated>0):
+        #     lossSeg=self.criterion(seg_hat, y_true)
+        #     lossReg=F.smooth_l1_loss(reg_hat,numLesions)
+        #     return torch.add(lossSeg,lossReg)
+        # return  F.smooth_l1_loss(reg_hat,numLesions)   
+        
+        # # y_hat, y , numLesions_ab= self.infer_batch_pos(batch)
+        # lossa = self.criterion(y_hat, y)
+        # # regressab=  self.modelRegression(y_hat)
+        # # numLesions_ab2=list(map(lambda entry : int(entry), numLesions_ab ))
+        # # numLesions_ab3=torch.Tensor(numLesions_ab2).to(self.device)  
+        # # lossab=F.smooth_l1_loss(torch.flatten(regressab), torch.flatten(numLesions_ab3) )
+      
+        # # # in case we have odd iteration we get access only to number of lesions present in the image not where they are (if they are present at all)    
+        # # y_hat_all, numLesions= self.infer_batch_all(batch)
+        # # regress_res=self.modelRegression(y_hat_all)
+        # # numLesions1=list(map(lambda entry : int(entry), numLesions ))
+        # # numLesions2=torch.Tensor(numLesions1).to(self.device)
+        # # # print(f" regress res {torch.flatten(regress_res).size()}  orig {torch.flatten(numLesions).size() } ")
+        # # lossb=F.smooth_l1_loss(torch.flatten(regress_res), torch.flatten(numLesions2) )
+
+        # # self.log('train_loss', torch.add(lossa,lossb), prog_bar=True)
+        # # self.log('train_image_loss', lossa, prog_bar=True)
+        # # self.log('train_reg_loss', lossb, prog_bar=True)
+        # # return torch.add(torch.add(lossa,lossb),lossab)
+        # return lossa
     # def validation_step(self, batch, batch_idx):
 
     def validation_step(self, batch, batch_idx):
-        images, y_true,numLesions= batch['chan3_col_name_val'], batch["label_name_val"], batch['num_lesions_to_retain']
-        #print(f" in validation images {images} labels {labels} "  )
-  
-        y_det = self.net(images)# sliding_window_inference(images, (32,32,32), 1, self.net)
-        #marking that we had some Nan numbers in the tensor
-        # if(torch.sum(torch.isnan( y_det))>0):
-        #     self.isAnyNan=True
+        x, y_true, numLesions,isAnythingInAnnotated = batch['chan3_col_name'], batch['label'], batch['num_lesions_to_retain'], batch['isAnythingInAnnotated']
+        seg_hat, reg_hat = self.modelRegression(x)
         
-        # total_loss= 0.0
-
-        # regress_res=self.modelRegression(y_det)
-
-        lossa = self.criterion(y_det, y_true)
-        # numLesions2= list(map(int, numLesions ))
-
-        # lossb=F.smooth_l1_loss(torch.flatten(regress_res).cpu(), torch.flatten(torch.tensor(numLesions2)).cpu() )
-
-        # val_losss=torch.add(lossa,lossb)    
+        val_losss= self.calculateLoss(isAnythingInAnnotated,seg_hat,y_true,reg_hat,numLesions)
 
 
-        y_det = decollate_batch(y_det)
+        y_det = decollate_batch(seg_hat)
         y_true = decollate_batch(y_true)
         #TODO probably this [1,:,:,:] could break the evaluation ...
         # y_det=[x.cpu().detach().numpy()[1,:,:,:][0] for x in y_det]
@@ -289,31 +284,32 @@ class Model(pl.LightningModule):
         # y_true= list(map(self.postTrue , y_det))
 
         dice = DiceMetric()
-        index=0
-        for i in range(0,len( y_det)):
-            #print(f"torch.flatten(regress_res)[i] {torch.flatten(regress_res)[i]}")
-            # regress_res_round= round(torch.flatten(regress_res)[i].item())
-            #print(f"pre  y_det[i] {y_det[i].size()} y_true_i {y_true[i].size()} ")
-            y_det_i=self.postProcess(y_det[i])[0,:,:,:].cpu()
-            y_true_i=self.postTrue(y_true[i])[1,:,:,:].cpu()
-            #print(f"post  y_det[i] {y_det_i.size()} y_true_i {y_true_i.size()} ")
-            if(torch.sum(y_det_i).item()>0 and torch.sum(y_true_i).item()>0 ):
-                # total_loss+= monai.metrics.compute_generalized_dice(y_det_i,y_true_i)/len( y_det)
-                #print(f" monai.metrics.compute_generalized_dice(y_det_i,y_true_i)/len( y_det) {monai.metrics.compute_generalized_dice(y_det_i,y_true_i)/len( y_det)} ")
-                dice(y_det_i,y_true_i)
-                #sd(y_pred=y_det_i, y=y_true_i) 
-            # print(f"numLesions[i] {numLesions[i]}")    
-            # total_loss+= (abs(regress_res_round-int(numLesions[i]) ) /len( y_det) )#arbitrary number
-        
-        # regress_res2= torch.flatten(regress_res) 
-        # regress_res3=list(map(lambda el:round(el) ,torch.flatten(regress_res2).cpu().detach().numpy() ))
+        if(isAnythingInAnnotated>0):
+            for i in range(0,len( y_det)):
+                #print(f"torch.flatten(regress_res)[i] {torch.flatten(regress_res)[i]}")
+                # regress_res_round= round(torch.flatten(regress_res)[i].item())
+                #print(f"pre  y_det[i] {y_det[i].size()} y_true_i {y_true[i].size()} ")
+                y_det_i=self.postProcess(y_det[i])[0,:,:,:].cpu()
+                y_true_i=self.postTrue(y_true[i])[1,:,:,:].cpu()
+                #print(f"post  y_det[i] {y_det_i.size()} y_true_i {y_true_i.size()} ")
+                if(torch.sum(y_det_i).item()>0 and torch.sum(y_true_i).item()>0 ):
+                    # total_loss+= monai.metrics.compute_generalized_dice(y_det_i,y_true_i)/len( y_det)
+                    #print(f" monai.metrics.compute_generalized_dice(y_det_i,y_true_i)/len( y_det) {monai.metrics.compute_generalized_dice(y_det_i,y_true_i)/len( y_det)} ")
+                    dice(y_det_i,y_true_i)
+                    #sd(y_pred=y_det_i, y=y_true_i) 
+                # print(f"numLesions[i] {numLesions[i]}")    
+                # total_loss+= (abs(regress_res_round-int(numLesions[i]) ) /len( y_det) )#arbitrary number
+            
+        regress_res2= torch.flatten(reg_hat) 
+        regress_res3=list(map(lambda el:round(el) ,torch.flatten(regress_res2).cpu().detach().numpy() ))
+
         # #print( f"torch.Tensor(numLesions).cpu() {torch.Tensor(numLesions).cpu()}  torch.Tensor(regress_res).cpu() {torch.Tensor(regress_res).cpu()}   ")
         # #self.F1Score(torch.Tensor(regress_res3).int(), torch.Tensor(numLesions2).cpu().int())
-        # total_loss=precision_recall(torch.Tensor(regress_res3).int(), torch.Tensor(numLesions2).cpu().int(), average='macro', num_classes=4)
-        # total_loss1=torch.mean(torch.stack([total_loss[0],total_loss[1]] ))#self.F1Score
-        # print(f" total loss a {total_loss1} val_loss {val_losss}")
-        # total_loss2= torch.add(total_loss1,dice.aggregate())
-        # print(f" total loss b {total_loss2}  total_loss,dice.aggregate() {dice.aggregate()}")
+        total_loss=precision_recall(torch.Tensor(regress_res3).int(), torch.Tensor(numLesions).cpu().int(), average='macro', num_classes=4)
+        total_loss1=torch.mean(torch.stack([total_loss[0],total_loss[1]] ))#self.F1Score
+        print(f" total loss a {total_loss1} val_loss {val_losss}")
+        total_loss2= torch.add(total_loss1,dice.aggregate())
+        print(f" total loss b {total_loss2}  total_loss,dice.aggregate() {dice.aggregate()}")
 
         # #print(f"sd.aggregate() {sd.aggregate().item()}")
         
@@ -322,8 +318,8 @@ class Model(pl.LightningModule):
         # self.log("validation_acc", total_loss2.item(), on_epoch=True, on_step=False, sync_dist=True, prog_bar=True, logger=True)
         # self.log("val_loss", val_losss.item(), on_epoch=True, on_step=False, sync_dist=True, prog_bar=True, logger=True)
        
-        # return {'val_acc': total_loss2.item(), 'val_loss':val_losss}
-        return {'val_acc': dice.aggregate().item(), 'val_loss':lossa}
+        return {'val_acc': total_loss2.item(), 'val_loss':val_losss}
+        #return {'val_acc': dice.aggregate().item(), 'val_loss':val_losss}
 
 
     def validation_epoch_end(self, outputs):
