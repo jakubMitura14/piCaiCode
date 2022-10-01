012be30e694915b9df5d8f3af00e1790c5f728c1
Jakub Mitura <jakub.mitura14@gmail.com>
2022-10-01 18:25:14 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- model/LigtningModel.py
+++ model/LigtningModel.py
@@ -358,29 +358,29 @@ class Model(pl.LightningModule):
         # reg_hat=np.rint(reg_hat.cpu().detach().numpy().flatten())
         # print(f" rrrrr {reg_hat}  ")
 
-        y_det=[extract_lesion_candidates( x.cpu().detach().numpy()[1,:,:,:])[0] for x in y_det]
-        y_true=[x.cpu().detach().numpy()[1,:,:,:] for x in y_true]
-        print("after extracting")
+#         y_det=[extract_lesion_candidates( x.cpu().detach().numpy()[1,:,:,:])[0] for x in y_det]
+#         y_true=[x.cpu().detach().numpy()[1,:,:,:] for x in y_true]
+#         print("after extracting")
         
-        pathssList=[]
-        # with mp.Pool(processes = mp.cpu_count()) as pool:
-        #     pathssList=pool.map(partial(save_candidates_to_dir,y_true=y_true,y_det=y_det,patIds=patIds,temp_val_dir=self.temp_val_dir,reg_hat=reg_hat),list(range(0,len(y_true))))
-        # forGoldVal=list(map(lambda tupl :tupl[0] ,pathssList  ))
-        # fory_hatVal=list(map(lambda tupl :tupl[1] ,pathssList  ))
+#         pathssList=[]
+#         # with mp.Pool(processes = mp.cpu_count()) as pool:
+#         #     pathssList=pool.map(partial(save_candidates_to_dir,y_true=y_true,y_det=y_det,patIds=patIds,temp_val_dir=self.temp_val_dir,reg_hat=reg_hat),list(range(0,len(y_true))))
+#         # forGoldVal=list(map(lambda tupl :tupl[0] ,pathssList  ))
+#         # fory_hatVal=list(map(lambda tupl :tupl[1] ,pathssList  ))
 
-        # self.list_gold_val=self.list_gold_val+forGoldVal
-        # self.list_yHat_val=self.list_gold_val+fory_hatVal
+#         # self.list_gold_val=self.list_gold_val+forGoldVal
+#         # self.list_yHat_val=self.list_gold_val+fory_hatVal
 
-# save_candidates_to_dir(y_true,y_det,patIds,i,temp_val_dir)
+# # save_candidates_to_dir(y_true,y_det,patIds,i,temp_val_dir)
         
-        for i in range(0,len(y_true)):
-            tupl=saveFilesInDir(y_true[i],y_det[i], self.temp_val_dir, patIds[i])
-            self.list_gold_val.append(tupl[0])
-            self.list_yHat_val.append(tupl[1])
-            # self.list_gold_val.append(forGoldVal[i])
-            # self.list_yHat_val.append(fory_hatVal[i])
+#         for i in range(0,len(y_true)):
+#             tupl=saveFilesInDir(y_true[i],y_det[i], self.temp_val_dir, patIds[i])
+#             self.list_gold_val.append(tupl[0])
+#             self.list_yHat_val.append(tupl[1])
+#             # self.list_gold_val.append(forGoldVal[i])
+#             # self.list_yHat_val.append(fory_hatVal[i])
 
-        self.log('val_loss', loss )
+#         self.log('val_loss', loss )
 
        # return {'loss' :loss,'loc_dice': diceVall }
 
@@ -423,7 +423,13 @@ class Model(pl.LightningModule):
 
     def validation_epoch_end(self, outputs):
         print("validation_epoch_end")
-        
+
+        tensorss = [torch.as_tensor(x['loc_dice']) for x in outputs]
+        if( len(tensorss)>0):
+            avg_dice = torch.mean(torch.stack(tensorss))
+
+            self.log('dice', avg_dice )
+
         #print(f" self.list_yHat_val {self.list_yHat_val} ")
         if(len(self.list_yHat_val)>1 and (not self.isAnyNan)):
             print("validation_epoch_end in   ")
@@ -487,7 +493,7 @@ class Model(pl.LightningModule):
         #     print(" naans in outputt  ")
 
         #self.isAnyNan=False
-        return {"mean_val_acc": self.log}
+        #return {"mean_val_acc": self.log}
 
 
         # # avg_loss = torch.mean(torch.stack([torch.as_tensor(x['val_loss']) for x in outputs]))
