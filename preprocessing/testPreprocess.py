98ba195916852347927c1fb90b0618e6bfceaa3c
Jakub Mitura <jakub.mitura14@gmail.com>
2022-10-04 11:11:27 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- preprocessing/testPreprocess.py
+++ preprocessing/testPreprocess.py
@@ -421,16 +421,16 @@ def preprocess_diffrent_spacings(df,targetSpacingg,spacing_keyword):
 
 
 
-#bias field correction
-#Standardize.iterateAndBiasCorrect('t2w',df)
-# #Standarization
-#for keyWord in ['t2w','adc', 'hbv']: #'cor',,'sag'
-    ## denoising
-    #Standardize.iterateAndDenoise(keyWord,df)
-    ## standarization
-    #Standardize.iterateAndStandardize(keyWord,df,trainedModelsBasicPath,80)   
-#standardize labels
-#Standardize.iterateAndchangeLabelToOnes(df)
+# bias field correction
+# Standardize.iterateAndBiasCorrect('t2w',df)
+#Standarization
+for keyWord in ['t2w','adc', 'hbv']: #'cor',,'sag'
+    # denoising
+    # Standardize.iterateAndDenoise(keyWord,df)
+    # standarization
+    Standardize.iterateAndStandardize(keyWord,df,trainedModelsBasicPath,80)   
+# standardize labels
+Standardize.iterateAndchangeLabelToOnes(df)
 
 #### 
 
