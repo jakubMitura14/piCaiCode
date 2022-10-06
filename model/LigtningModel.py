30f34fab371f7916b01572b78d69ec1f4de92a3b
Jakub Mitura <53857487+jakubMitura14@users.noreply.github.com>
2022-10-06 13:23:33 +0200

    up

--- model/LigtningModel.py
+++ model/LigtningModel.py
@@ -583,7 +583,7 @@ class Model(pl.LightningModule):
             # dices=list(map(partial(calcDiceFromPaths,list_yHat_val=self.list_yHat_val,list_gold_val=self.list_gold_val   ),list(range(0,len(self.list_yHat_val)))))
             # meanDice=torch.mean(torch.stack( dices)).item()
             
-            self.log('meanDice',np.mean( np.array(self.dices).flatten()) )
+            self.log('meanDice',torch.mean(torch.stack( dices)).item() )
             print('meanDice',np.mean( np.array(self.dices ).flatten()))
             # self.log('mean_surface_distance',torch.mean(torch.stack( self.surfDists)).item())
 
