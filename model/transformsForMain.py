8b1f9dba916be761af36139d95b0f7e89ff9ac77
Jakub Mitura <jakub.mitura14@gmail.com>
2022-10-09 16:00:14 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- model/transformsForMain.py
+++ model/transformsForMain.py
@@ -187,7 +187,7 @@ def get_val_transforms(is_whole_to_train,spatial_size):
             # Spacingd(keys=["chan3_col_name","label"]], pixdim=(
             #     1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
             #SpatialPadd(keys=["chan3_col_name","label"],spatial_size=maxSize) ,
-            DivisiblePadd(keys=["chan3_col_name_val","label_name_val"],k=32) ,
+            #DivisiblePadd(keys=["chan3_col_name_val","label_name_val"],k=32) ,
             ResizeWithPadOrCropd(keys=["chan3_col_name","label_name_val"],spatial_size=spatial_size ),
             SelectItemsd(keys=["chan3_col_name_val","label_name_val","study_id","num_lesions_to_retain","isAnythingInAnnotated"]),
             #*decide_if_whole_image_train(is_whole_to_train,"chan3_col_name_val","label_name_val"),
