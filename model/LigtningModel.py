aac249b1f6b01ad0f82cb1aecb34094a434866d4
Jakub Mitura <jakub.mitura14@gmail.com>
2022-10-13 10:52:09 +0200

    u[
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- model/LigtningModel.py
+++ model/LigtningModel.py
@@ -461,8 +461,8 @@ class Model(pl.LightningModule):
         with mp.Pool(processes = mp.cpu_count()) as pool:
             #it = pool.imap(my_task, range(lenn))
             results = list(map(lambda i: pool.apply_async(my_task, (i,)) ,list(range(lenn))  ))
-            # time.sleep(TIMEOUT)
-            processedCases=list(map(lambda ind :getNext(ind,results,60) ,list(range(lenn)) ))
+            time.sleep(70)
+            processedCases=list(map(lambda ind :getNext(ind,results,10) ,list(range(lenn)) ))
 
         extracteds=list(filter(lambda it:type(it) != type(None),processedCases))
         # extracteds=list(filter(lambda it:it.numpy(),extracteds))
@@ -506,51 +506,55 @@ class Model(pl.LightningModule):
             return {'dices': diceLoc, 'meanPiecaiMetr_auroc':meanPiecaiMetr_auroc
                     ,'meanPiecaiMetr_AP' :meanPiecaiMetr_AP,'meanPiecaiMetr_score': meanPiecaiMetr_score}
 
+        return {'dices': 0.0, 'meanPiecaiMetr_auroc':0.0
+                ,'meanPiecaiMetr_AP' :0.0,'meanPiecaiMetr_score': 0.0}
+
 
 
 
     def validation_epoch_end(self, outputs):
         print("validation_epoch_end")
-        print(f"outputs {outputs[0]['dices'] }")
-        allDices = np.array(([x['dices'].cpu().detach().numpy() for x in outputs])).flatten() 
-        allmeanPiecaiMetr_auroc = np.array(([x['meanPiecaiMetr_auroc'].cpu().detach().numpy() for x in outputs])).flatten() 
-        allmeanPiecaiMetr_AP = np.array(([x['meanPiecaiMetr_AP'].cpu().detach().numpy() for x in outputs])).flatten() 
-        allmeanPiecaiMetr_score = np.array(([x['meanPiecaiMetr_score'].cpu().detach().numpy() for x in outputs])).flatten() 
-        
-      
-        
-        
-        if(len(allDices)>0):            
-            # allforEval = torch.stack([item for sublist in allforEval for item in sublist]).numpy()
-            # print(f"allforEval b {allforEval}")
-            # meanPiecaiMetr_auroc,meanPiecaiMetr_AP,meanPiecaiMetr_score= evaluate_all_cases(allforEval)
-                    # for i in range(0,numIters):
-            # valid_metrics = evaluate(y_det=allextracted,
-            #                         y_true=allgold,
-            #                         num_parallel_calls= os.cpu_count()
-            #                         ,verbose=1
-            #                            )
-
-            meanPiecaiMetr_auroc=np.nanmean(allmeanPiecaiMetr_auroc)
-            meanPiecaiMetr_AP=np.nanmean(allmeanPiecaiMetr_AP)
-            meanPiecaiMetr_score= np.nanmean(allmeanPiecaiMetr_score)
-            # print("finished evaluating")
+        # print(f"outputs {outputs[0]['dices'] }")
+        if(len(outputs)>0):
+            allDices = np.array(([x['dices'].cpu().detach().numpy() for x in outputs])).flatten() 
+            allmeanPiecaiMetr_auroc = np.array(([x['meanPiecaiMetr_auroc'].cpu().detach().numpy() for x in outputs])).flatten() 
+            allmeanPiecaiMetr_AP = np.array(([x['meanPiecaiMetr_AP'].cpu().detach().numpy() for x in outputs])).flatten() 
+            allmeanPiecaiMetr_score = np.array(([x['meanPiecaiMetr_score'].cpu().detach().numpy() for x in outputs])).flatten() 
             
-            # if(len(allDices)>0):
-            #     self.log('dice', np.mean(allDices))
+        
+            
+            
+            if(len(allDices)>0):            
+                # allforEval = torch.stack([item for sublist in allforEval for item in sublist]).numpy()
+                # print(f"allforEval b {allforEval}")
+                # meanPiecaiMetr_auroc,meanPiecaiMetr_AP,meanPiecaiMetr_score= evaluate_all_cases(allforEval)
+                        # for i in range(0,numIters):
+                # valid_metrics = evaluate(y_det=allextracted,
+                #                         y_true=allgold,
+                #                         num_parallel_calls= os.cpu_count()
+                #                         ,verbose=1
+                #                            )
 
-            # print(f"meanPiecaiMetr_auroc {meanPiecaiMetr_auroc} meanPiecaiMetr_AP {meanPiecaiMetr_AP}  meanPiecaiMetr_score {meanPiecaiMetr_score} "  )
+                meanPiecaiMetr_auroc=np.nanmean(allmeanPiecaiMetr_auroc)
+                meanPiecaiMetr_AP=np.nanmean(allmeanPiecaiMetr_AP)
+                meanPiecaiMetr_score= np.nanmean(allmeanPiecaiMetr_score)
+                # print("finished evaluating")
+                
+                # if(len(allDices)>0):
+                #     self.log('dice', np.mean(allDices))
 
-            self.log('val_mean_auroc', meanPiecaiMetr_auroc)
-            self.log('val_mean_AP', meanPiecaiMetr_AP)
-            self.log('mean_val_acc', meanPiecaiMetr_score)
-            # # tensorss = [torch.as_tensor(x['loc_dice']) for x in outputs]
-            # # if( len(tensorss)>0):
-            #     avg_dice = torch.mean(torch.stack(tensorss))
+                # print(f"meanPiecaiMetr_auroc {meanPiecaiMetr_auroc} meanPiecaiMetr_AP {meanPiecaiMetr_AP}  meanPiecaiMetr_score {meanPiecaiMetr_score} "  )
 
-            self.picaiLossArr_auroc_final.append(meanPiecaiMetr_auroc)
-            self.picaiLossArr_AP_final.append(meanPiecaiMetr_AP)
-            self.picaiLossArr_score_final.append(meanPiecaiMetr_score)
+                self.log('val_mean_auroc', meanPiecaiMetr_auroc)
+                self.log('val_mean_AP', meanPiecaiMetr_AP)
+                self.log('mean_val_acc', meanPiecaiMetr_score)
+                # # tensorss = [torch.as_tensor(x['loc_dice']) for x in outputs]
+                # # if( len(tensorss)>0):
+                #     avg_dice = torch.mean(torch.stack(tensorss))
+
+                self.picaiLossArr_auroc_final.append(meanPiecaiMetr_auroc)
+                self.picaiLossArr_AP_final.append(meanPiecaiMetr_AP)
+                self.picaiLossArr_score_final.append(meanPiecaiMetr_score)
 
  
 
