2e134c837adc996e78a8c4669cf29935b70e5f30
Jakub Mitura <jakub.mitura14@gmail.com>
2022-10-15 20:23:38 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- model/DataModule.py
+++ model/DataModule.py
@@ -285,9 +285,10 @@ class PiCaiDataModule(pl.LightningDataModule):
         # self.train_ds_no_labels = SmartCacheDataset(data=noLabels, transform=train_transforms_noLabel  ,num_init_workers=os.cpu_count(),num_replace_workers=os.cpu_count())
 
         # self.train_ds_all =  LMDBDataset(data=train_set_all, transform=train_transforms,cache_dir=self.persistent_cache)
-
-        self.val_ds=  Dataset(data=onlyPositiveSubjects[0:25]+onlyNegative[0:10], transform=val_transforms )
-        self.train_ds_labels = Dataset(data=onlyPositiveSubjects[25:]+onlyNegative[10:], transform=train_transforms )
+        onlyPosTreshold=5
+        onlyNegativeThreshold=5
+        self.val_ds=  Dataset(data=onlyPositiveSubjects[0:onlyPosTreshold]+onlyNegative[0:onlyNegativeThreshold], transform=val_transforms )
+        self.train_ds_labels = Dataset(data=onlyPositiveSubjects[onlyPosTreshold:]+onlyNegative[onlyNegativeThreshold:], transform=train_transforms )
         self.train_ds_no_labels = Dataset(data=noLabels, transform=train_transforms_noLabel)
 
 
