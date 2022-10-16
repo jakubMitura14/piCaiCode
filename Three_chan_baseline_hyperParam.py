61e9230398b05061c5e3def42b83297eeff2b511
Jakub Mitura <jakub.mitura14@gmail.com>
2022-10-16 06:42:34 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- Three_chan_baseline_hyperParam.py
+++ Three_chan_baseline_hyperParam.py
@@ -125,7 +125,7 @@ def getUnetD(dropout,input_image_size,in_channels,out_channels ):
         in_channels=in_channels,
         out_channels=out_channels,
         strides=[(1, 1, 1),(2, 2, 2), (1, 2, 2), (1, 2, 2), (1, 2, 2), (2, 2, 2),(1, 1, 1)],
-        channels=[32, 64, 128, 256, 512, 1024,2048,4096],
+        channels=[32, 64, 128, 256, 512, 1024,2048,2048],
         num_res_units= 0,
         act = (Act.PRELU, {"init": 0.2}),
         norm= (Norm.BATCH, {}),
